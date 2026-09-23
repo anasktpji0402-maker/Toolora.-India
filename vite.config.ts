3e4e06ab946af694673f49685935a61f219ebc13
anasktpji0402-maker <anasktpji0402@gmail.com>
2026-09-23 15:53:22 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,2 @@
+import {defineConfig} from 'vite'; import react from '@vitejs/plugin-react'; import {VitePWA} from 'vite-plugin-pwa';
+export default defineConfig({plugins:[react(),VitePWA({registerType:'autoUpdate',includeAssets:['favicon.svg'],manifest:{name:'TOOLORA',short_name:'TOOLORA',description:'Everyday tools. One simple place.',theme_color:'#0b1220',background_color:'#f7f9fc',display:'standalone',start_url:'/',icons:[{src:'/icons/icon-192.svg',sizes:'192x192',type:'image/svg+xml'},{src:'/icons/icon-512.svg',sizes:'512x512',type:'image/svg+xml'}]}})]});
